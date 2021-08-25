5ab9f45f3d73186cfe92f3d397d2ce27261be7d7
Miguel Gagliardo <migag9@gmail.com>
2021-08-25 21:36:37 +0200

    Agregando clase 19-08-2021

---
+++ 19-08-2021/EsPar.java
@@ -0,0 +1,20 @@
+// Programa que toma un input (int) e imprime si el numero ingresado es par o no
+
+import java.util.Scanner;
+
+public class EsPar {
+    public static void main(String args[]) {
+        System.out.println ("Ingrese un numero: ");
+
+        Scanner input = new Scanner(System.in);
+        int numeroIntroducido = input.nextInt();
+        input.close()
+
+        if (numeroIntroducido % 2 == 0) {
+            System.out.println("El numero ingresado es par.");
+        }
+        else {
+            System.out.println("El numero ingresado no es par.");
+        }
+    }
+}
